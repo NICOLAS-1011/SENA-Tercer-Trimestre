88ce5536e5f85726e397615fd24fe761a7de870c
NICOLAS-1011 <nicolasandresespitiavillamil@gmail.com>
2022-09-17 12:30:21 -0500

    front de api

--- Septiembre 13 - API/front.js
+++ Septiembre 13 - API/front.js
@@ -1,32 +1,32 @@
 console.log('Hola gatos')
 
-const API_URL='https://api.thecatapi.com/v1/images/search?limit=1'
-// fetch(URL)
-// .then(res=>res.json())
-// .then(data=>{
-//     const img=document.querySelector('img');
-//     img.src=data[0].url;
-//});
+const API_URL='https://random.dog/woof.json/'
+const API_URL2='https://random.dog/woof.json/'
+const API_URL3='https://random.dog/woof.json/'
 
 async function reload(){
     const res=await fetch(API_URL);
     const data=await res.json();
     console.log(data)
 
+    const res2=await fetch(API_URL2);
+    const data2=await res2.json();
+    console.log(data2)
+
+    const res3=await fetch(API_URL3);
+    const data3=await res3.json();
+    console.log(data3)
 
 
-    //----------------------
+
     const img1=document.getElementById('img1');
-    //----------------------
+    img1.src=data.url;
 
+    const img2=document.getElementById('img2');
+    img2.src=data2.url;      
 
-
-
-    // const img2=document.getElementById('img2');
-    // const img3=document.getElementById('img3');
-    img1.src=data.url;        
-    // img2.src=data[1].url;        
-    // img3.src=data[2].url;        
+    const img3=document.getElementById('img3');
+    img3.src=data3.url;      
 }
 
 reload();
